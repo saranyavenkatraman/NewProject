6997cf8163e1e65ea51bf6d7366d83a7e68e44c2
saranyavenkatraman <saranyavenkatraman.mk@gmail.com>
2022-01-23 09:46:50 +0530

    some code added

--- src/org/sample/GreensDetails.java
+++ src/org/sample/GreensDetails.java
@@ -1,5 +1,13 @@
 package org.sample;
 
 public class GreensDetails {
+	
+	private void greensLocation() {
 
+		System.out.println("Greens Main Location:Chennai");
+	}
+
+	private void greensAnotherLocation() {
+System.out.println("Anther location:Tambaram");
+	}
 }
